19143d1942a222783ca51aee790a14a2c512331c
Karen <jcalderonzambrano2@gmail.com>
2022-02-08 16:46:25 -0500

    añadir

---
+++ js/holamundo.js
@@ -0,0 +1,30 @@
+//"use strict" Primer ejercicio
+//console.log("Hola mundo");
+
+
+//"use strict"
+
+//console.log("Bienvenido s(ra) Karen");
+
+//"use strict"
+//let nombre = "Karen";
+//let genero = "Femenino";
+//let genero2= "Masculino";
+
+//if (genero == "Femenino") {
+    //console.log ("Bienvenida señora " + nombre)
+//}
+//else if(genero2 == "Masculino") {
+    //console.log("Bienvenido" + nombre)
+//}
+//else(
+    //console.log("Defina su genero")
+//)
+
+"use strict"
+
+
+
+
+
+
