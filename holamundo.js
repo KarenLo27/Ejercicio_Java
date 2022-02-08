cc62b9876e74cc042039e683fbb41bca19b606a9
Karen <jcalderonzambrano2@gmail.com>
2022-02-08 14:36:38 -0500

    hola

---
+++ holamundo.js
@@ -0,0 +1,15 @@
+//"use strict" Primer ejercicio
+//console.log("Hola mundo");
+
+
+//"use strict"
+//console.log("Bienvenido s(ra) Karen");
+
+"use strict"
+var nombre = "Karen"
+var genero = "Femenino"
+if 
+
+
+
+
